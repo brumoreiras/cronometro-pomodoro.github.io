2574b2027805d4de9d868a3c7f54403bfec12a0e
Bruno Moreira Santos <62727781+brumoreiras@users.noreply.github.com>
2022-12-19 19:43:23 -0300

    Iniciando o projeto

---
+++ js/main.js
@@ -0,0 +1,54 @@
+let min = document.querySelector('#minutos');
+let seg = document.querySelector('#segundos');
+let milis = document.querySelector('#milissegundos');
+
+let contadorMili = 0
+let contadorSeg = 0
+let contadorMin = 0
+let INTERVALO
+
+function contaMilissegundos() {
+    contadorMili++
+    if (contadorMili < 10) {
+        milis.innerHTML = '0' + contadorMili
+    } else {
+        milis.innerHTML = contadorMili
+    }
+    if (contadorMili == 99) {
+        contadorMili = 0
+        contaSegundos()
+    }
+}
+
+function contaSegundos() {
+    contadorSeg++
+    if (contadorSeg < 59) {
+        seg.innerHTML = '0' + contadorSeg
+    } else {
+        seg.innerHTML = contadorSeg
+    }
+    if (contadorSeg == 59) {
+        contadorSeg = 0
+        contaMinutos()
+    }
+}
+
+function contaMinutos() {
+    contadorMin++
+    if (contadorMin < 10) {
+        min.innerHTML = '0' + contadorMin
+    } else {
+        min.innerHTML = contadorMin
+    }
+}
+
+function iniciar() {
+    clearInterval(INTERVALO)
+    INTERVALO = setInterval(() => {
+        contaMilissegundos()
+    }, 10)
+}
+
+function stop(){
+    clearInterval(INTERVALO)
+}
